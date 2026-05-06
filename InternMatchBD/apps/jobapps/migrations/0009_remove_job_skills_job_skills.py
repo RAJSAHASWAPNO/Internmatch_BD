c14deebe1754774f6af7749f6a9812a3823087a5
smashpop7 <officl.ahmed@gmail.com>
2026-05-07 01:33:05 +0600

    add migrations in jobapps

---
+++ InternMatchBD/apps/jobapps/migrations/0009_remove_job_skills_job_skills.py
@@ -0,0 +1,20 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('jobapp', '0008_rename_category_skill_remove_job_category_job_skills'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='job',
+            name='skills',
+        ),
+        migrations.AddField(
+            model_name='job',
+            name='skills',
+            field=models.ManyToManyField(blank=True, related_name='jobs', to='jobapp.skill'),
+        ),
+    ]
